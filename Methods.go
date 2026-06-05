0d22c96c91f0d51db0339d87b0ceb3e160fba375
seshu.manasa <seshu.manasa@techmojo.in>
2026-06-05 11:59:18 +0530

    absolute method

--- Methods.go
+++ Methods.go
@@ -1,3 +1,21 @@
 package main
 
-import
+import (
+	"fmt"
+	"math"
+)
+
+type Myfloat float64
+
+func (f Myfloat) Absval() Myfloat {
+	if f<0{
+		return -f 
+	}
+
+	return f
+}
+
+func main() {
+	f:= Myfloat(-math.Sqrt(13))
+	fmt.Println(f.Absval())
+}
